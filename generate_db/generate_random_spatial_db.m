f91da6e9181a788ac42339c3086cf0b4f9e87aa0
AdeNurhopipah <87224261+AdeNurhopipah@users.noreply.github.com>
2025-02-04 11:19:20 +1300

    Update generate_random_spatial_db.m

--- generate_db/generate_random_spatial_db.m
+++ generate_db/generate_random_spatial_db.m
@@ -2,8 +2,6 @@
 % Sub: Example on spatial domain using ERSP and ERP 
 % Experiment design from: 
 % Ravindan, A.S. & Conteras-Vidal J (2023) 
-% "An Empirical Comparison of Deep Learning Explainability Approaches for EEG
-% using Simulated Ground Truth"
 
 
 function generate_random_spatial(class, SNR)
