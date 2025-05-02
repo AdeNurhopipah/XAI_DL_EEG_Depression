e44a52fd1d7732089686fe5906e829626eb21cb9
AdeNurhopipah <87224261+AdeNurhopipah@users.noreply.github.com>
2025-05-02 13:27:49 +1200

    Update generate_random_spectral_db.m

--- generate_db/generate_random_spectral_db.m
+++ generate_db/generate_random_spectral_db.m
@@ -1,6 +1,6 @@
 % Generate simulated dataset 
 % Sub: Example on the spectral domain using ERSP 
-% Based on the design from: Ravindran, A.S. & Contreras-Vidal, J.L. (2023)
+% Based on the design from: Ravindran, A.S. & Contreras-Vidal, J. (2023)
 
 % Author: Ade Nurhopipah
 % Email: ade.nurhopipah@postgrad.otago.ac.nz
