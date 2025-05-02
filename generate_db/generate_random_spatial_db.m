3353aebba23661cd482a559a71f550851eeff917
AdeNurhopipah <87224261+AdeNurhopipah@users.noreply.github.com>
2025-05-02 13:26:31 +1200

    Update generate_random_spatial_db.m

--- generate_db/generate_random_spatial_db.m
+++ generate_db/generate_random_spatial_db.m
@@ -1,6 +1,7 @@
-% Generate simulated dataset 
-% Sub: Example on the spatial domain using ERSP and ERP 
-% Experiment design from: Ravindan, A.S. & Conteras-Vidal J (2023) 
+% Generate simulated EEG dataset
+% Example: Spatial domain using combined ERSP and ERP features
+% Based on the design from: Ravindran, A.S. & Contreras-Vidal, J.L. (2023)
+
 % Author: Ade Nurhopipah
 % Email: ade.nurhopipah@postgrad.otago.ac.nz
 
